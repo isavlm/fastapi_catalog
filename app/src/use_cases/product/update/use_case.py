f50fec026a909d51d73e33779b92c6ed3fd9d2e1
Isadora V <isa@Isadoras-MacBook-Air.local>
2024-12-29 17:08:45 -0800

    Finished with update/edit route. Refactored the code for Delete as well.

--- app/src/use_cases/product/update/use_case.py
+++ app/src/use_cases/product/update/use_case.py
@@ -1,41 +1,45 @@
-from .request import UpdateProductRequest
-from .response import UpdateProductResponse
-#from app.src.repositories import ProductRepository
-
-from typing import Any, Optional
-
+from typing import Optional
+from fastapi.exceptions import HTTPException
 from app.src.exceptions import (
     ProductNotFoundException,
-    ProductRepositoryException,
+    ProductRepositoryException
 )
 
 from app.src.core.models import Product
 from app.src.repositories import ProductRepository
 
+from .request import UpdateProductRequest
+from .response import UpdateProductResponse
+
 
 class UpdateProduct:
-    def __init__(self, product_repository: ProductRepository) -> None:
+    def __init__(self, product_repository: ProductRepository):
         self.product_repository = product_repository
-
-    def __verify_product_exists(
-        self, product: Optional[Product], request_entity_id: str
-    ) -> None:
-        if product is None:
-            raise ProductNotFoundException(product_id=request_entity_id)
 
     def __call__(
         self, product_id: str, request: UpdateProductRequest
-    ) -> UpdateProductResponse:
-        print("Product Updated Successfully:")
+    ) -> Optional[UpdateProductResponse]:
         try:
-            existing_product = self.product_repository.get_by_id(product_id)
-            print(existing_product)
-            self.__verify_product_exists(
-                existing_product, request_entity_id=request.product_id
-            )
-            response = self.product_repository.Update(request)
-            
-            return response
+            with self.product_repository.session() as session:
+                existing_product = self.product_repository.get_by_id(
+                    product_id, session=session
+                )
+                self.__verify_product_exists(
+                    existing_product, request_entity_id=request.product_id
+                )
+                response = self.product_repository.update(
+                    product_id, request, session=session
+                )
+                return response
+        except ProductNotFoundException as e:
+            raise HTTPException(status_code=404, detail=str(e))
         except ProductRepositoryException as e:
-            raise e 
-        
+            raise HTTPException(status_code=500, detail=str(e))
+
+    def __verify_product_exists(
+        self, existing_product: Optional[Product], request_entity_id: str
+    ) -> None:
+        if existing_product is None:
+            raise ProductNotFoundException(
+                f"Product with ID {request_entity_id} not found"
+            )
